7e005da9dbc2c086fcd104ecf020880c80569a74
AlejandroFlores004 <alejandroflores004@outlook.com>
2024-10-01 11:01:00 -0600

    Pagina start base

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.start)
+    path('',views.start),
+    path('login/',views.login)
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def start(request):
+    if request.method == 'POST':
+        return redirect('login/')
     return render(request,'start.html')
+
+def login(request):
+    return render(request,'login.html')
